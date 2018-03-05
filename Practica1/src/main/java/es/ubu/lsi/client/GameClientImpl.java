c83f974065d666a40f85bbcf766e9badb8ecb519
rnc0011 <rnc0011@alu.ubu.es>
2018-03-05 16:13:37 +0100

    Primeros cambios

--- Practica1/src/main/java/es/ubu/lsi/client/GameClientImpl.java
+++ Practica1/src/main/java/es/ubu/lsi/client/GameClientImpl.java
@@ -73,4 +73,23 @@ public class GameClientImpl {
 		}
 
 	}
+
+	/**
+	 * Clase GameClientListener.
+	 * 
+	 * @author Raúl Negro Carpintero
+	 * @author Mario Núñez Izquierdo
+	 * @version 1.0
+	 *
+	 */
+	public class GameClientListener implements Runnable {
+
+		/**
+		 * Método run.
+		 */
+		public void run() {
+
+		}
+
+	}
 }
